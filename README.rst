dd891c1ac25902b457187b77cd8e362a72068107
Mark Fine <mark.fine@gmail.com>
2015-04-01 11:45:07 -0700

    Relocate to piksi_tools

---
+++ README.rst
@@ -0,0 +1,46 @@
+Tools for the Piksi GNSS receiver
+=================================
+
+Python tools for the Piksi GNSS receiver.
+
+Setup
+-----
+
+Install dependencies only::
+
+  $ sudo pip install -r requirements.txt
+
+Install from repo::
+
+  $ sudo pip setup.py install
+
+Install package from pypi::
+
+  $ sudo pip install piksi_tools
+
+Usage Examples
+--------------
+
+Bootloader example
+~~~~~~~~~~~~~~~~~~
+
+Something cool.
+
+Console example
+~~~~~~~~~~~~~~~
+
+Something cooler.
+
+Testing
+-------
+
+To run the tests and check for coverage::
+
+  $  py.test -v --cov piksi_tools tests/
+
+License
+-------
+
+Copyright © 2015 Swift Navigation
+
+Distributed under LGPLv3.0.
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from piksi_tools import __version__
+from setuptools import setup
+import os
+
+CLASSIFIERS = [
+  'Intended Audience :: Developers',
+  'Intended Audience :: Science/Research',
+  'Operating System :: POSIX :: Linux',
+  'Operating System :: MacOS :: MacOS X',
+  'Operating System :: Microsoft :: Windows',
+  'Programming Language :: Python',
+  'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+  'Topic :: Software Development :: Libraries :: Python Modules',
+  'Programming Language :: Python :: 2.7',
+]
+
+PACKAGES = [
+  'piksi_tools',
+]
+
+PLATFORMS = [
+  'linux',
+  'osx',
+  'win32',
+]
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+with open(cwd + '/README.rst') as f:
+  readme = f.read()
+
+with open(cwd + '/requirements.txt') as f:
+  INSTALL_REQUIRES = [i.strip() for i in f.readlines()]
+
+setup(name='piksi_tools',
+      description='Python tools for the Piksi GNSS receiver.',
+      long_description=readme,
+      version=__version__,
+      author='Swift Navigation',
+      author_email='dev@swiftnav.com',
+      url='https://github.com/swift-nav/piksi_tools',
+      classifiers=CLASSIFIERS,
+      packages=PACKAGES,
+      platforms=PLATFORMS,
+      install_requires=INSTALL_REQUIRES,
+      use_2to3=False,
+      zip_safe=False)
