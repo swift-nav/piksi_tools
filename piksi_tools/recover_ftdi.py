7ef2c4296f2881ce4a6921410353e55d79795285
Mark Fine <mark.fine@gmail.com>
2015-04-01 17:53:41 -0700

    more files.

--- piksi_tools/recover_ftdi.py
+++ piksi_tools/recover_ftdi.py
@@ -20,63 +20,58 @@ import sys
 from sbp.piksi import SBP_MSG_SETTINGS, SBP_MSG_SETTINGS_SAVE
 
 def send_setting(link, section, name, value):
-  link.send_message(SBP_MSG_SETTINGS, '%s\0%s\0%s\0' % (section, name, value))
+  link.send(SBP_MSG_SETTINGS, '%s\0%s\0%s\0' % (section, name, value))
 
 
-if __name__ == "__main__":
+def get_args():
+  """
+  Get and parse arguments.
+  """
+  import argparse
   parser = argparse.ArgumentParser(description='Piksi Recover FTDI')
   parser.add_argument('-p', '--port',
-                      default=[serial_link.DEFAULT_PORT], nargs=1,
+                      default=[serial_link.SERIAL_PORT], nargs=1,
                       help='specify the serial port to use.')
   parser.add_argument("-b", "--baud",
-                      default=[serial_link.DEFAULT_BAUD], nargs=1,
+                      default=[serial_link.SERIAL_BAUD], nargs=1,
                       help="specify the baud rate to use.")
   parser.add_argument("-f", "--ftdi",
                       help="use pylibftdi instead of pyserial.",
                       action="store_true")
-  args = parser.parse_args()
-  serial_port = args.port[0]
+  return parser.parse_args()
+
+def main():
+  """
+  Get configuration, get driver, and build handler and start it.
+  """
+  port = args.port[0]
   baud = args.baud[0]
-  print "Waiting for device to be plugged in ...",
-  sys.stdout.flush()
-  found_device = False
-  while not found_device:
-    try:
-      link = serial_link.SerialLink(serial_port, baud=baud, use_ftdi=args.ftdi,
-                                    print_unhandled = False)
-      found_device = True
-    except KeyboardInterrupt:
-      # Clean up and exit
-      link.close()
-      sys.exit()
-    except:
-      # Couldn't find device
-      time.sleep(0.01)
-  print "link successfully created."
+  # Driver with context
+  with serial_link.get_driver(args.ftdi, port, baud) as driver:
+    # Handler with context
+    with Handler(driver.read, driver.write) as link:
+      print "Resetting mask to 0xff"
+      send_setting(link, "uart_ftdi", "sbp_message_mask", "65535")
+      time.sleep(0.5)
 
-  print "Resetting mask to 0xff"
-  send_setting(link, "uart_ftdi", "sbp_message_mask", "65535")
-  time.sleep(0.5)
+      print "Resetting baudrate to 1Mbps"
+      send_setting(link, "uart_ftdi", "baudrate", "1000000")
+      time.sleep(0.5)
 
-  print "Resetting baudrate to 1Mbps"
-  send_setting(link, "uart_ftdi", "baudrate", "1000000")
-  time.sleep(0.5)
+      print "Resetting mode to SBP"
+      send_setting(link, "uart_ftdi", "mode", "SBP")
+      time.sleep(0.5)
 
-  print "Resetting mode to SBP"
-  send_setting(link, "uart_ftdi", "mode", "SBP")
-  time.sleep(0.5)
+      print "Attempting to save settings"
+      link.send(SBP_MSG_SETTINGS_SAVE, "")
+      time.sleep(0.5)
+      link.send(SBP_MSG_SETTINGS_SAVE, "")
+      time.sleep(0.5)
+      link.send(SBP_MSG_SETTINGS_SAVE, "")
+      time.sleep(0.5)
+      link.send(SBP_MSG_SETTINGS_SAVE, "")
 
-  print "Attempting to save settings"
-  link.send_message(SBP_MSG_SETTINGS_SAVE, "")
-  time.sleep(0.5)
-  link.send_message(SBP_MSG_SETTINGS_SAVE, "")
-  time.sleep(0.5)
-  link.send_message(SBP_MSG_SETTINGS_SAVE, "")
-  time.sleep(0.5)
-  link.send_message(SBP_MSG_SETTINGS_SAVE, "")
+      print "Sent Settings Reset message to return FTDI to defaults"
 
-  print "Sent Settings Reset message to return FTDI to defaults"
-
-  # Clean up and exit
-  link.close()
-  sys.exit()
+if __name__ == "__main__":
+  main()
