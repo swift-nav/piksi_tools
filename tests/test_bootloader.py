37a74dd6e19067172264f06719bb3fc2c39c665e
Colin Beighley <colinbeighley@gmail.com>
2015-06-17 17:10:36 -0700

    Add license and copyright header

--- tests/test_bootloader.py
+++ tests/test_bootloader.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
+# Copyright (C) 2011-2014 Swift Navigation Inc.
+# Contact: Colin Beighley <colin@swiftnav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
 
 import unittest
 import sys
