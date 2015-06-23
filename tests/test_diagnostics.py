3e0b4dfe1f43bccae62fc22f4876a7078794dacf
Bhaskar Mookerji <mookerji@gmail.com>
2015-06-23 15:22:37 -0700

    Script to check firmware/nap settings on test.
    HITL test doesn't always bootload the right firmware. Testing scripts
    now check versions and exit early if versions aren't what we expected.
    
    Required for:
    https://github.com/swift-nav/sbp_log_analysis/issues/196
    
    /cc @mfine @cbeighley @denniszollo

---
+++ tests/test_diagnostics.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Copyright (C) 2015 Swift Navigation Inc.
+# Contact: Bhaskar Mookerji <mookerji@swiftnav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+import piksi_tools.diagnostics as d
+import pytest
+
+
+def test_diag_check():
+  details = 'tests/data/device_details.yaml'
+  version = 'fw: v0.17-27-ge2c1aac\nhdl: v0.13-rc0\n'
+  assert d.check_diagnostics(details, version)
+  version = 'fw: v0.17-27-deadbeef\nhdl: v0.13-rc0\n'
+  assert not d.check_diagnostics(details, version)
+  version = 'fw: v0.17-27-ge2c1aac\nhdl: v0.13-defecated\n'
+  assert not d.check_diagnostics(details, version)
+  with pytest.raises(Exception):
+    assert not d.check_diagnostics(details, None)
