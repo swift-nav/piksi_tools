d3b08a35c72e2e223e13dd30ef96b1859ea37e0d
Colin Beighley <colinbeighley@gmail.com>
2015-07-01 10:26:09 -0700

    Add Heartbeat to discover SBP version

--- piksi_tools/stm_unique_id.py
+++ piksi_tools/stm_unique_id.py
@@ -14,37 +14,38 @@ import struct
 import sys
 import serial_link
 
-from piksi_tools.heartbeat import Heartbeat
-from sbp.system import SBP_MSG_HEARTBEAT
-
+from sbp.system         import SBP_MSG_HEARTBEAT
 from sbp.flash          import SBP_MSG_STM_UNIQUE_ID_REQUEST, \
                                SBP_MSG_STM_UNIQUE_ID_RESPONSE
 from sbp.client.handler import *
+
+from timeout import *
+from heartbeat import Heartbeat
 
 class STMUniqueID(object):
   """
   Retrieve the STM Unique ID from Piksi.
   """
 
-  def __init__(self, link):
+  def __init__(self, link, sbp_version):
     """
     Parameters
     ==========
     link : sbp.client.handler.Handler
       link to register Heartbeat message callback with
+    sbp_version : tuple (int, int)
+      SBP version to use for STM Unique ID messages.
     """
     self.unique_id_returned = False
     self.unique_id = None
     self.link = link
-    self.heartbeat = Heartbeat()
-    link.add_callback(self.heartbeat, SBP_MSG_HEARTBEAT)
-    link.add_callback(self.receive_stm_unique_id_callback, SBP_MSG_STM_UNIQUE_ID_RESPONSE)
+    self.sbp_version = sbp_version
 
   def __enter__(self):
+    self.link.add_callback(self.receive_stm_unique_id_callback, SBP_MSG_STM_UNIQUE_ID_RESPONSE)
     return self
 
   def __exit__(self, *args):
-    self.link.remove_callback(self.heartbeat, SBP_MSG_HEARTBEAT)
     self.link.remove_callback(self.receive_stm_unique_id_callback, SBP_MSG_STM_UNIQUE_ID_RESPONSE)
 
   def receive_stm_unique_id_callback(self, sbp_msg):
@@ -57,11 +58,10 @@ class STMUniqueID(object):
 
   def get_id(self):
     """ Retrieve the STM Unique ID. Blocks until it has received the ID. """
-    while not self.heartbeat.received:
-      time.sleep(0.1)
     self.unique_id_returned = False
     self.unique_id = None
-    # < 0.45 of the bootloader, reuse single stm message.
+    # < SBP v0.45, reuse single message.
+    print self.sbp_version
     if self.sbp_version < (0, 45):
       self.link.send(SBP_MSG_STM_UNIQUE_ID_RESPONSE, struct.pack("<I",0))
     else:
@@ -97,7 +97,13 @@ def main():
   # Driver with context
   with serial_link.get_driver(args.ftdi, port, baud) as driver:
     with Handler(driver.read, driver.write) as link:
-      with STMUniqueID(link) as stm_unique_id:
+      heartbeat = Heartbeat()
+      link.add_callback(heartbeat, SBP_MSG_HEARTBEAT)
+      with Timeout(TIMEOUT_BOOT) as timeout:
+        while not heartbeat.received:
+          time.sleep(0.1)
+      link.remove_callback(heartbeat, SBP_MSG_HEARTBEAT)
+      with STMUniqueID(link, heartbeat.sbp_version) as stm_unique_id:
         unique_id = stm_unique_id.get_id()
       print "STM Unique ID =", "0x" + ''.join(["%02x" % (b) for b in unique_id])
 
