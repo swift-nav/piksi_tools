ba5204ac753ae09cb4fe30fdb9f70e2d78b0fdc6
Gareth McMullin <gareth@blacksphere.co.nz>
2015-07-02 09:50:34 -0700

    coredump: take elf file as argument.

--- piksi_tools/testing/grab_core_on_segfault.sh
+++ piksi_tools/testing/grab_core_on_segfault.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Usage: ./grab_core_on_segfault.sh <piksi_dev> <bmp_dev> <seconds-to-sleep>
+# Usage: ./grab_core_on_segfault.sh <firmeare.elf> \
+#                                   <piksi_dev> <bmp_dev> \
+#                                   <seconds-to-sleep>
 #
 # Intended to be called from HITL log capture:
 # if [ -e $MD_EXTERNAL_RESOURCES_LOCKED_BMP1 ]; then
-#   ./grab_core_on_segfault.sh $MD_EXTERNAL_RESOURCES_LOCKED_PORT1 \
+#   ./grab_core_on_segfault.sh ../../piksi_firmware_$GIT_DESCRIBE.elf \
+#                              $MD_EXTERNAL_RESOURCES_LOCKED_PORT1 \
 #                              $MD_EXTERNAL_RESOURCES_LOCKED_BMP1 \
 #                              $SECONDS
 # fi
@@ -13,23 +16,24 @@ set -e
 
 [ -e $1 ]
 [ -e $2 ]
+[ -e $3 ]
 
 # Find serial number from Piksi device name
-export PIKSI=`echo $1 | egrep -o "PK[0-9]{4}"`
+export PIKSI=`echo $2 | egrep -o "PK[0-9]{4}"`
 
 gdb-multiarch -batch -nx \
-              -ex "tar ext $2" \
+              -ex "tar ext $3" \
               -ex "source coredump3.py" \
               -ex "set gcore-file-name core-$PIKSI" \
               -ex "mon jtag 4 5 6" \
               -ex "att 1" \
               -ex "run" \
-              ~/Projects/piksi_firmware/build/piksi_firmware.elf \
+              $1 \
               &>gdblog.$PIKSI &
 
 sleep 1
 trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
 tail -f gdblog.$PIKSI | grep "core dumped" &
 
-sleep $3
+sleep $4
 
