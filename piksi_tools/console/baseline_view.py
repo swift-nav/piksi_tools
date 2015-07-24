f42336f34306d0f932cdb06d7f9be5d6b2b02a25
Gareth McMullin <gareth@blacksphere.co.nz>
2015-07-24 13:44:00 -0700

    console: Square axes when zoom all selected.

--- piksi_tools/console/baseline_view.py
+++ piksi_tools/console/baseline_view.py
@@ -16,6 +16,7 @@ from chaco.tools.api import ZoomTool, PanTool
 from enable.api import ComponentEditor
 from enable.savage.trait_defs.ui.svg_button import SVGButton
 from pyface.api import GUI
+from utils import plot_square_axes
 
 import struct
 import math
@@ -49,6 +50,7 @@ class BaselineView(HasTraits):
   plot_data = Instance(ArrayPlotData)
 
   running = Bool(True)
+  zoomall = Bool(False)
   position_centered = Bool(False)
 
   clear_button = SVGButton(
@@ -57,7 +59,7 @@ class BaselineView(HasTraits):
     width=16, height=16
   )
   zoomall_button = SVGButton(
-    label='', tooltip='Zoom All',
+    label='', tooltip='Zoom All', toggle=True,
     filename=os.path.join(os.path.dirname(__file__), 'images', 'iconic', 'fullscreen.svg'),
     width=16, height=16
   )
@@ -100,10 +102,7 @@ class BaselineView(HasTraits):
   )
 
   def _zoomall_button_fired(self):
-    self.plot.index_range.low_setting = 'auto'
-    self.plot.index_range.high_setting = 'auto'
-    self.plot.value_range.low_setting = 'auto'
-    self.plot.value_range.high_setting = 'auto'
+    self.zoomall = not self.zoomall
 
   def _center_button_fired(self):
     self.position_centered = not self.position_centered
@@ -253,6 +252,8 @@ class BaselineView(HasTraits):
       d = (self.plot.value_range.high - self.plot.value_range.low) / 2.
       self.plot.value_range.set_bounds(soln.n - d, soln.n + d)
 
+    if self.zoomall:
+      plot_square_axes(self.plot, ('e_fixed', 'e_float'), ('n_fixed', 'n_float'))
     self.table = table
 
   def __init__(self, link, plot_history_max=1000):
---
+++ piksi_tools/console/utils.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2011-2014 Swift Navigation Inc.
+# Contact: Gareth McMullin <gareth@swift-nav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+import sys
+import traceback
+
+def plot_square_axes(plot, xnames, ynames):
+  try:
+    if type(xnames) is str:
+      xs = plot.data.get_data(xnames)
+      ys = plot.data.get_data(ynames)
+      minx = min(xs); maxx = max(xs)
+      miny = min(ys); maxy = max(ys)
+    else:
+      minx = min(min(plot.data.get_data(xname)) for xname in xnames)
+      maxx = max(max(plot.data.get_data(xname)) for xname in xnames)
+      miny = min(min(plot.data.get_data(yname)) for yname in ynames)
+      maxy = max(max(plot.data.get_data(yname)) for yname in ynames)
+    rangex = maxx - minx
+    rangey = maxy - miny
+    try:
+      aspect = float(plot.width) / plot.height
+    except: aspect = 1
+    if aspect * rangey > rangex:
+      padding = (aspect * rangey - rangex) / 2
+      plot.index_range.low_setting = minx - padding
+      plot.index_range.high_setting = maxx + padding
+      plot.value_range.low_setting = miny
+      plot.value_range.high_setting = maxy
+    else:
+      padding = (rangex / aspect - rangey) / 2
+      plot.index_range.low_setting = minx
+      plot.index_range.high_setting = maxx
+      plot.value_range.low_setting = miny - padding
+      plot.value_range.high_setting = maxy + padding
+    dy = plot.value_range.high_setting - plot.value_range.low_setting
+    dx = plot.index_range.high_setting - plot.index_range.low_setting
+  except Exception as e:
+    sys.__stderr__.write(traceback.format_exc() + '\n')
+
