70b9a88a0745752b0d11d831936660f3d2f3d1a0
dzollo <dzollo@swift-nav.com>
2015-08-07 13:03:14 -0700

    docstrings and formatting cleanup output_list

--- piksi_tools/console/output_list.py
+++ piksi_tools/console/output_list.py
@@ -11,7 +11,8 @@
 
 """Contains the class OutputStream, a HasTraits file-like text buffer."""
 
-from traits.api import HasTraits, Str, Bool, Trait, Int, List, Font, Float, Enum, Property
+from traits.api import HasTraits, Str, Bool, Trait, Int, List, Font, Float, \
+                       Enum, Property
 from traitsui.api import View, UItem, TabularEditor
 from pyface.api import GUI
 from traitsui.tabular_adapter import TabularAdapter
@@ -27,23 +28,22 @@ LOG_NOTICE     = 5       # normal but significant condition
 LOG_INFO       = 6       # informational
 LOG_DEBUG      = 7       # debug-level messages
 
-# These log levels are defined uniquely to this module to handle our overload of the
+# These log levels are defined uniquely to this module to handle the
 # list for stdout, stderror, and the device's log messages
-LOG_STD_ERR    = 8       # Python Std Error
-LOG_STD_OUT    = 9       # Special Placeholder for Standard Output messages from Python
+# An unknown log level will end up with - 2
+# A python stdout or stderr should come in as - 1
 
 LOG_LEVEL_CONSOLE = -1
 LOG_LEVEL_DEFAULT = -2
 
-# This maps the numbers to a human readable string
-# The unused log levels are commented out of the dict
-# Because the filter drop-down uses all values in the dict.
-# We may need these if we extend loglevels
+# This maps the log level numbers to a human readable string
+# The unused log levels are commented out of the dict until used
 
-# These log levels will be unmaskable
+# OTHERLOG_LEVELS will be unmaskable
 OTHERLOG_LEVELS  = {
                     LOG_LEVEL_CONSOLE: "CONSOLE",
                     }
+# SYSLOG_LEVELS will be maskable
 
 SYSLOG_LEVELS = {#LOG_EMERG : "EMERG",
                  #LOG_ALERT : "ALERT",
@@ -53,14 +53,13 @@ SYSLOG_LEVELS = {#LOG_EMERG : "EMERG",
                  #LOG_NOTICE: "NOTICE",
                  LOG_INFO  : "INFO",
                  LOG_DEBUG : "DEBUG",
-# These log levels are defined uniquely to this module to handle our overload of the
-# list for stdout, stderror, and the device's log messages
-                 #LOG_STD_ERR: "PYTHON_STD_ERROR",
-                 #LOG_STD_OUT: "PYTHON_STD_OUTPUT"
                  }
 
+# Combine the log levels into one dict
 ALL_LOG_LEVELS = SYSLOG_LEVELS.copy()
 ALL_LOG_LEVELS.update(OTHERLOG_LEVELS)
+
+# Set default filter level
 DEFAULT_LOG_LEVEL_FILTER = "WARNING"
 
 DEFAULT_MAX_LEN = 250
@@ -69,7 +68,8 @@ class LogItemOutputListAdapter(TabularAdapter):
   """
   Tabular adapter for table of log iteritems
   """
-  columns = [('Timestamp', 'timestamp'), ('Log level', 'log_level_str'), ('Message', 'msg')]
+  columns = [('Timestamp', 'timestamp'), ('Log level', 'log_level_str'),
+             ('Message', 'msg')]
   font = Font('12')
   can_edit = Bool(False)
   timestamp_width = Float(0.18)
@@ -78,14 +78,35 @@ class LogItemOutputListAdapter(TabularAdapter):
   can_drop = Bool(False)
 
 class LogItem(HasTraits):
+  """
+  This class handles items in the list of log entries
+  Parameters
+  ----------
+  log_level  : int
+    integer representing the log leve
+  timestamp : str
+    the time that the console read the log item
+  msg : str
+    the text of the message
+  """
   log_level = Int
   timestamp = Str
   msg = Str
-  # log level string maps the int into a the string via the global SYSLOG_LEVELS dict
-  # If we can't find the int in the dict, we assume it is of type "CONSOLE" from standard out/ err
+
+  # log level string maps the int into a the string via the global ALL_LOG_LEVELS dict
+  # If we can't find the int in the dict, we print "UNKNOWN"
   log_level_str = Property(fget=lambda self: ALL_LOG_LEVELS.get(self.log_level, "UNKNOWN"),
                    depends_on='log_level')
   def __init__(self, msg, level=None):
+    """
+    Constructor for logitem
+    Notes:
+    ----------
+    Timestamp initailzies to current system time
+    msg is passed in by the user
+    If level is passed in as NONE, an attempt is made to infer a log level
+    from the msg
+    """
     self.log_level = LOG_LEVEL_DEFAULT
     if level == None: # try to infer log level if an old message
       split_colons = msg.split(":")
@@ -97,7 +118,9 @@ class LogItem(HasTraits):
           self.log_level = key
     else:
       self.log_level = level
+    # remove line breaks from the message
     self.msg = msg.rstrip('\n')
+    # set timestamp
     self.timestamp = time.strftime("%b %d %Y %H:%M:%S")
 
   def matches_log_level_filter(self, log_level):
@@ -130,25 +153,35 @@ class OutputList(HasTraits):
   copied from the paused buffer to the main text string.
   """
 
-  # The text that has been written with the 'write' method.
+  # Holds LogItems to display
   unfiltered_list = List(LogItem)
+  # Holds LogItems while self.paused is True.
+  _paused_buffer = List(LogItem)
+  # filtered set of messages
   filtered_list = List(LogItem)
+  # state of fiter on messages
   log_level_filter = Enum(list(SYSLOG_LEVELS.iterkeys()))
   # The maximum allowed length of self.text (and self._paused_buffer).
   max_len = Trait(DEFAULT_MAX_LEN, None, Int)
 
-  # When True, the 'write' method appends its value to self._paused_buffer
-  # instead of self.text.  When the value changes from True to False,
-  # self._paused_buffer is copied back to self.text.
+  # When True, the 'write' or 'write_level' methods append to self._paused_buffer
+  # When the value changes from True to False, self._paused_buffer is copied
+  # back to self.unfiltered_list.
   paused = Bool(False)
 
-  # String that holds text written while self.paused is True.
-  _paused_buffer = List(LogItem)
+
 
   def write(self, s):
     """
-    Write to the list, this method exist to allow STDERR and STDOUT to be redirected to the list
-    Assumption this is only called when writing to STDOUT and STDERR
+    Write to the lists OutputList as STDOUt or STDERR.
+    This method exist to allow STDERR and STDOUT to be redirected into this
+    display. It should only be called when writing to STDOUT and STDERR.
+    Ignores spaces.
+
+    Parameters
+    ----------
+    s : str
+      string to cast as LogItem and write to tables
     """
 
     if not s.isspace():
@@ -161,6 +194,17 @@ class OutputList(HasTraits):
           self.append_truncate(self.filtered_list, log)
 
   def write_level(self, s, level=None):
+    """
+    Write to the lists in OutputList from device or user space.
+
+    Parameters
+    ----------
+    s : str
+      string to cast as LogItem and write to tables
+    level : int
+      Integer log level to use when creating log item.  If this is none, LogItem
+      constructer will attempt to infer a log level from the string.
+    """
     log = LogItem(s, level)
     if self.paused:
       self.append_truncate(self._paused_buffer, log)
@@ -171,7 +215,7 @@ class OutputList(HasTraits):
 
   def append_truncate(self, buffer, s):
     if len(buffer) > self.max_len:
-      assert (len(buffer) - self.max_len) == 1, "buffer got longer and never was truncated"
+      assert (len(buffer) - self.max_len) == 1, "Output list buffer is too long"
       buffer.pop()
     buffer.insert(0, s)
 
@@ -187,20 +231,24 @@ class OutputList(HasTraits):
     pass
 
   def _log_level_filter_changed(self):
-    self.filtered_list = [item for item in self.unfiltered_list if item.matches_log_level_filter(self.log_level_filter)]
-    #self.filtered_list = filter(LogItem.matches_log_level_filter(self.log_level_filter), self.unfiltered_list)
+    self.filtered_list = [item for item in self.unfiltered_list \
+                          if item.matches_log_level_filter(self.log_level_filter)]
 
 
   def _paused_changed(self):
+    """
+    Method to call when the paused boolean changes state.
+    We need to handle the multiple copies of the buffers.
+    """
     if self.paused:
-      # Copy the current text to _paused_buffer.  While the OutputStream
-      # is paused, the write() method will append its argument to _paused_buffer.
+      # Copy the current list to _paused_buffer.  While the OutputStream
+      # is paused, the write methods will append its argument to _paused_buffer.
       self._paused_buffer = self.unfiltered_list
     else:
-      # No longer paused, so copy the _paused_buffer to the displayed text, and
+      # No longer paused, so copy the _paused_buffer to the displayed list, and
       # reset _paused_buffer.
       self.unfiltered_list = self._paused_buffer
-      # have to refilter the filtered list too
+      # we have to refilter the filtered list too
       self._log_level_filter_changed()
       self._paused_buffer = []
 
