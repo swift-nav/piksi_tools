e1c6e80b04cf951e761f904c347073d74a5cc7b8
dzollo <dzollo@swift-nav.com>
2015-08-20 10:50:05 -0700

    mavlink to pandas dataframe initial commit.  Slight issue with timezone on msg indexes

---
+++ piksi_tools/ardupilot/mavlink2pandas.py
@@ -0,0 +1,82 @@
+from mavlink_decode import extractSBP
+import os
+import pandas as pd
+import tempfile
+from pymavlink.DFReader import DFReader_binary, DFMessage
+import datetime
+
+def extractMAVLINK(filename, outfile, msg_types_to_save):
+  """
+  From dataflashfile create a dict of PANDAS dataframes for each msg
+
+  """
+  log = DFReader_binary(filename)
+  last_m = None
+  num_msgs = 0
+  out_dict = {}
+  first = True
+  init_time = 0
+  while True:
+    # we use mavlinks recv_match function to iterate through logs
+    m = log.recv_match(type=msg_types_to_save)
+    if m:
+      #print type(m)
+      timestamp = m._timestamp
+      if first:
+        init_time = timestamp
+      delta = timestamp - init_time
+      dt = datetime.datetime.utcfromtimestamp(timestamp)
+      msg_timestamp_dict = out_dict.get(m.get_type(),{})
+      msg_timestamp_dict[dt] = m.to_dict()
+      # print msg_timestamp_dict
+      out_dict[m.get_type()] = msg_timestamp_dict
+    else:
+      if os.path.exists(outfile):
+        print "Unlinking %s, which already exists!" % outfile
+        os.unlink(outfile)
+      f = pd.HDFStore(outfile, mode='w')
+      try:
+        tabs = [key for key in out_dict.iterkeys()]
+        for tab in tabs:
+          attr = out_dict[tab]
+          f.put(tab, pd.DataFrame(attr))
+          if f.get(tab).empty:
+            warnings.warn('%s is empty.' % tab)
+      finally:
+        f.close()
+      break
+    #print out_dict[m.get_type()]
+
+
+def get_args():
+  """
+  Get and parse arguments.
+
+  """
+  import argparse
+  parser = argparse.ArgumentParser(description='Mavlink to Pandas table')
+  parser.add_argument("dataflashfile",
+                      help="the dataflashfile to convert.")
+  parser.add_argument('-o', '--outfile',
+                      default=["mavlink_convert.hdf5"], nargs=1,
+                      help='specify the name of the file output.')
+  parser.add_argument('-t', '--types',
+                      default=['GPS','GPS2'], nargs='+',
+                      help='specify the mavlink messages to convert.')
+  args = parser.parse_args()
+  return args
+
+
+
+
+
+
+def main():
+  args = get_args()
+  filename = args.dataflashfile
+  outfile = args.outfile[0]
+  if extractMAVLINK(filename, outfile, args.types):
+    print "Mavlink log succesfully written to {0}.".format(outfile)
+
+if __name__ == "__main__":
+  main()
