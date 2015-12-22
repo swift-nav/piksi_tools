6f0241dcf428c9fcacac628cac02adf0f40ca9b8
dzollo <dzollo@swift-nav.com>
2015-12-21 16:35:42 -0800

    allow external PIL in OSX bootstrapping script

--- tasks/setup.sh
+++ tasks/setup.sh
@@ -147,6 +147,7 @@ function install_python_deps_osx () {
     brew install qt pyqt libftdi pyside
     sudo pip install chaco==4.4.1 enable==4.4.1 intelhex==1.5 \
          --allow-unverified enable \
+         --allow-external PIL \
          --allow-unverified PIL \
          --allow-external intelhex \
          --allow-unverified intelhex
