cd0bf4d40b4ba7b831593bff61ae3129604a50cd
dzollo <dzollo@swift-nav.com>
2016-06-17 20:05:57 -0700

    add dead simple unit test

---
+++ tests/test_sbp2rinex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# Copyright (C) 2015 Swift Navigation Inc.
+# Contact: Bhaskar Mookerji <mookerji@swiftnav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+import piksi_tools.sbp2rinex as s2r
+import bitstring as bs
+import pytest
+import os
+
+
+
+def test_function():
+  s2r.wrapper("./tests/data/sbp2rinex_data", '/tmp/out', None)
+  assert os.path.exists('/tmp/out')
+
