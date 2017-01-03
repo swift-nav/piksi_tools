79491b8560755c25ae305b956f922dff5d20f584
dzollo <dzollo@swift-nav.com>
2017-01-03 15:06:19 -0800

    add script to setup udev rules for piksi v2 and piksi multi

---
+++ tasks/configure_udev_rules.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+current_user="$USER"
+echo "Creating new udev rule 99-piksi.rules with the following lines"
+sudo tee /etc/udev/rules.d/99-piksi.rules <<EOF
+ATTRS{idProduct}=="6014", ATTRS{idVendor}=="0403", MODE="666", GROUP="dialout"
+ATTRS{idProduct}=="8398", ATTRS{idVendor}=="0403", MODE="666", GROUP="dialout"
+ATTRS{idProduct}=="A4A7", ATTRS{idVendor}=="0525", MODE="666", GROUP="dialout"
+EOF
+echo "Adding current user to the dialout group."
+sudo usermod -a -G dialout $current_user 
+echo "reloading udev rules"
+sudo udevadm control --reload-rules
