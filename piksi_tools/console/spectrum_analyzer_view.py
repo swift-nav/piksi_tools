49df65aa66c4d7d88680c0ce92e099e8a351596a
Margaret Sy <margaretsy1016@gmail.com>
2017-02-21 13:45:10 -0800

    make empty view for spectrum analyzer

---
+++ piksi_tools/console/spectrum_analyzer_view.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Copyright (C) 2011-2014, 2017 Swift Navigation Inc.
+# Contact: engineering@swiftnav.com
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+from traits.api import HasTraits, Str, Dict
+from traitsui.api import Item, Spring, View
+from pyface.api import GUI
+
+class SpectrumAnalyzerView(HasTraits):
+  python_console_cmds = Dict()
+  dummy_text = Str('This is where the Spectrum Analyzer output will go.')
+
+  traits_view = View(
+    Item('dummy_text', label='description')
+  )
+
+  def spectrum_analyzer_state_callback(self, sbp_msg, **metadata):
+    raise NotImplementedError
+
+  def update_plot(self):
+    raise NotImplementedError
+
+  def __init__(self, link):
+    super(SpectrumAnalyzerView, self).__init__()
+    self.python_console_cmds = {
+      'spectrum': self
+    }
