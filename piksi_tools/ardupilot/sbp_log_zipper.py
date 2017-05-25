d35b6abcd0c31a828e022d061b706ad7ea4bf230
Niels Joubert <njoubert@gmail.com>
2017-05-24 17:32:46 -0700

    SBP Log Zipper bugfix ensures compatibility with GLONASS

--- piksi_tools/ardupilot/sbp_log_zipper.py
+++ piksi_tools/ardupilot/sbp_log_zipper.py
@@ -55,7 +55,7 @@ def extract_gpstime(msg, last_gpstime=(0,0)):
   if msg.msg_type == ob.SBP_MSG_OBS:
     return (msg.header.t.wn, msg.header.t.tow)
   elif msg.msg_type == ob.SBP_MSG_EPHEMERIS_GPS or msg.msg_type == ob.SBP_MSG_EPHEMERIS_SBAS or msg.msg_type == ob.SBP_MSG_EPHEMERIS_GLO:
-    return (msg.toc.wn, msg.toc.tow)
+    return (msg.common.toe.wn, msg.common.toe.tow)
   elif msg.msg_type == ob.SBP_MSG_IONO:
     return (msg.t_nmct.wn, msg.t_nmct.tow)
   elif msg.msg_type == ob.SBP_MSG_BASE_POS_ECEF or msg.msg_type == ob.SBP_MSG_BASE_POS_LLH:
