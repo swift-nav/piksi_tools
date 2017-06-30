dc7a3e6ccc91889c233765c65b1da51055efe470
Keerthan Jaic <keerthan@swiftnav.com>
2017-06-29 17:06:17 -0700

    travis: run flake8

---
+++ tasks/travis.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+if [[ $TESTENV ]]; then
+  tox -e $TESTENV
+else
+  tox
+  rm -rf ~/home/.enthought
+  PYTHONPATH=. python piksi_tools/console/console.py --file --error -p ./tests/data/piksi.bin & PID=$!
+  sleep 10 
+  kill -n 9  $PID
+  PYTHONPATH=. python piksi_tools/console/console.py --file --error -p ./tests/data/20170513-180207.1.1.26.bin & PID=$!
+  sleep 10 
+  kill -n 9  $PID
+  make build_console
+  sudo python setup.py install
+fi
