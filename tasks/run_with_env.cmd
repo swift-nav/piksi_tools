19c3d4b7c5cf1da7f532adf0f5134a34346d4bc4
Keerthan Jaic <keerthan@swiftnav.com>
2017-08-08 16:09:26 -0700

    appveyor: try using win sdk

--- tasks/run_with_env.cmd
+++ tasks/run_with_env.cmd
@@ -83,6 +83,10 @@ IF %PYTHON_ARCH% == 64 (
     )
 ) ELSE (
     ECHO Using default MSVC build environment for 32 bit architecture
+        SET DISTUTILS_USE_SDK=1
+        SET MSSdk=1
+        "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
+        "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x86 /release
     ECHO Executing: %COMMAND_TO_RUN%
     call %COMMAND_TO_RUN% || EXIT 1
 )
