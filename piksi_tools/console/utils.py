9eafc58a4ca28d36b52e303e1f22195316ab5442
dzollo <dzollo@swift-nav.com>
2018-01-23 18:34:41 -0800

    reorder signals to avoid whitespace in gui

--- piksi_tools/console/utils.py
+++ piksi_tools/console/utils.py
@@ -23,54 +23,54 @@ from sbp.navigation import (SBP_MSG_BASELINE_NED, SBP_MSG_BASELINE_NED_DEP_A,
 # EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 
-CODE_GPS_L1CA  =  0
-CODE_GPS_L2CM  =  1
-CODE_GPS_L2CL  =  7
-CODE_GPS_L2CX  =  8
-CODE_GPS_L1P   =  5
-CODE_GPS_L2P   =  6
-CODE_GPS_L5I   =  9
-CODE_GPS_L5Q   = 10
-CODE_GPS_L5X   = 11
+CODE_GPS_L1CA = 0
+CODE_GPS_L2CM = 1
+CODE_GPS_L2CL = 7
+CODE_GPS_L2CX = 8
+CODE_GPS_L1P = 5
+CODE_GPS_L2P = 6
+CODE_GPS_L5I = 9
+CODE_GPS_L5Q = 10
+CODE_GPS_L5X = 11
 
-CODE_GLO_L1OF  =  3
-CODE_GLO_L2OF  =  4
+CODE_GLO_L1OF = 3
+CODE_GLO_L2OF = 4
 
-CODE_SBAS_L1CA =  2
+CODE_SBAS_L1CA = 2
 
-CODE_BDS2_B11  = 12
-CODE_BDS2_B2   = 13
+CODE_BDS2_B11 = 12
+CODE_BDS2_B2 = 13
 
-CODE_GAL_E1B   = 14
-CODE_GAL_E1C   = 15
-CODE_GAL_E1X   = 16
-CODE_GAL_E6B   = 17
-CODE_GAL_E6C   = 18
-CODE_GAL_E6X   = 19
-CODE_GAL_E7I   = 20
-CODE_GAL_E7Q   = 21
-CODE_GAL_E7X   = 22
-CODE_GAL_E8    = 23
-CODE_GAL_E5I   = 24
-CODE_GAL_E5Q   = 25
-CODE_GAL_E5X   = 26
+CODE_GAL_E1B = 14
+CODE_GAL_E1C = 15
+CODE_GAL_E1X = 16
+CODE_GAL_E6B = 17
+CODE_GAL_E6C = 18
+CODE_GAL_E6X = 19
+CODE_GAL_E7I = 20
+CODE_GAL_E7Q = 21
+CODE_GAL_E7X = 22
+CODE_GAL_E8 = 23
+CODE_GAL_E5I = 24
+CODE_GAL_E5Q = 25
+CODE_GAL_E5X = 26
 
-CODE_QZS_L1CA  = 27
-CODE_QZS_L2CM  = 28
-CODE_QZS_L2CL  = 29
-CODE_QZS_L2CX  = 30
-CODE_QZS_L5I   = 31
-CODE_QZS_L5Q   = 32
-CODE_QZS_L5X   = 33
+CODE_QZS_L1CA = 27
+CODE_QZS_L2CM = 28
+CODE_QZS_L2CL = 29
+CODE_QZS_L2CX = 30
+CODE_QZS_L5I = 31
+CODE_QZS_L5Q = 32
+CODE_QZS_L5X = 33
 
 SUPPORTED_CODES = [CODE_GPS_L1CA,
                    CODE_GPS_L2CM,
-                   CODE_GPS_L5Q,
                    CODE_GLO_L1OF,
                    CODE_GLO_L2OF,
                    CODE_SBAS_L1CA,
                    CODE_BDS2_B11,
                    CODE_BDS2_B2,
+                   CODE_GPS_L5Q,
                    CODE_QZS_L1CA,
                    CODE_QZS_L2CM]
 
@@ -83,8 +83,8 @@ L2P_STR = 'GPS L2P'
 SBAS_STR = 'SBAS L1'
 L1OF_STR = 'GLO L1OF'
 L2OF_STR = 'GLO L2OF'
-BDS_B1_STR  = 'BDS B1'
-BDS_B2_STR   = 'BDS B2'
+BDS_B1_STR = 'BDS B1'
+BDS_B2_STR = 'BDS B2'
 QZS_L1CA_STR = 'QZSS L1CA'
 QZS_L2CM_STR = 'QZSS L2CM'
 QZS_L2CX_STR = 'QZSS L2C'
@@ -181,14 +181,18 @@ def code_is_glo(code):
 
 sbas_codes = {CODE_SBAS_L1CA}
 
+
 def code_is_sbas(code):
     return code in sbas_codes
+
 
 bds2_codes = {CODE_BDS2_B11,
               CODE_BDS2_B2}
 
+
 def code_is_bds2(code):
     return code in bds2_codes
+
 
 qzss_codes = {CODE_QZS_L1CA,
               CODE_QZS_L2CM,
@@ -199,8 +203,10 @@ qzss_codes = {CODE_QZS_L1CA,
               CODE_QZS_L5X
               }
 
+
 def code_is_qzss(code):
     return code in qzss_codes
+
 
 def get_mode(msg):
     mode = msg.flags & 0x7
@@ -217,8 +223,8 @@ def get_mode(msg):
         elif mode == 2:
             mode = 3
     elif msg.msg_type not in [
-            SBP_MSG_BASELINE_NED_DEP_A, SBP_MSG_POS_LLH_DEP_A, SBP_MSG_POS_LLH,
-            SBP_MSG_BASELINE_NED
+        SBP_MSG_BASELINE_NED_DEP_A, SBP_MSG_POS_LLH_DEP_A, SBP_MSG_POS_LLH,
+        SBP_MSG_BASELINE_NED
     ]:
         print("called get_mode with unsupported message type: {0}".format(
             msg.msg_type))
@@ -236,9 +242,9 @@ def log_time_strings(week, tow):
        needed
     """
     if week is not None and tow > 0:
-        t_gps = datetime.datetime(1980, 1, 6) + \
-            datetime.timedelta(weeks=week) + \
-            datetime.timedelta(seconds=tow)
+        t_gps = (datetime.datetime(1980, 1, 6) +
+                 datetime.timedelta(weeks=week) +
+                 datetime.timedelta(seconds=tow))
         (t_gps_date, t_gps_secs) = datetime_2_str(t_gps)
     else:
         t_gps_date = ""
