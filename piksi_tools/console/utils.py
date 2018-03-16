265ebf86d2dac1782f6c236d9e192631386edb73
Benjamin Altieri <benaltieri@gmail.com>
2018-03-16 14:49:50 -0700

    Change to format strings

--- piksi_tools/console/utils.py
+++ piksi_tools/console/utils.py
@@ -241,9 +241,9 @@ def datetime_2_str(datetm):
 def sizeof_fmt(num, suffix='B'):
     for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
         if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
+            return "{:3.1f}{}{}".format(num, unit, suffix)
         num /= 1024.0
-    return "%.1f%s%s" % (num, 'Y', suffix)
+    return "{:.1f}{}{}".format(num, 'Y', suffix)
 
 
 def log_time_strings(week, tow):
