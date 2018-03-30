1c44e9ceed315dcb6cbacbf0e917e063d064682a
denniszollo <dzollo@swift-nav.com>
2018-03-30 08:53:10 -0700

    Update configure_udev_rules.sh

--- tasks/configure_udev_rules.sh
+++ tasks/configure_udev_rules.sh
@@ -5,7 +5,7 @@ sudo tee /etc/udev/rules.d/99-piksi.rules <<EOF
 ATTRS{idProduct}=="6014", ATTRS{idVendor}=="0403", MODE="666", GROUP="dialout"
 ATTRS{idProduct}=="8398", ATTRS{idVendor}=="0403", MODE="666", GROUP="dialout"
 ATTRS{idProduct}=="A4A7", ATTRS{idVendor}=="0525", MODE="666", GROUP="dialout"
-ATTRS{idProduct}=="1001", ATTRS{idVendor}=="2e69", ATTRS{bInterfaceNumber}=="02", MODE="666", GROUP="dialout", SYMLINK+="PiksiMultiUSB%n" 
+SUBSYSTEM=="tty", ENV{ID_VENDOR_ID}=="2e69", ENV{ID_MODEL_ID}=="1001", ENV{ID_USB_INTERFACE_NUM}=="00", MODE="666", GROUP="dialout", SYMLINK+="PiksiMultiUSB%n"
 EOF
 echo "Adding current user to the dialout group."
 sudo usermod -a -G dialout $current_user 
