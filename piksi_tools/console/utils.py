69f613f9bb93c139618cf9d928d07f211dc459ee
Leith Bade <leith@swiftnav.com>
2018-05-15 19:36:34 +1000

    Add support for missing signals, order signals consistently

--- piksi_tools/console/utils.py
+++ piksi_tools/console/utils.py
@@ -44,6 +44,9 @@ CODE_BDS2_B2 = 13
 CODE_GAL_E1B = 14
 CODE_GAL_E1C = 15
 CODE_GAL_E1X = 16
+CODE_GAL_E5I = 24
+CODE_GAL_E5Q = 25
+CODE_GAL_E5X = 26
 CODE_GAL_E6B = 17
 CODE_GAL_E6C = 18
 CODE_GAL_E6X = 19
@@ -51,9 +54,6 @@ CODE_GAL_E7I = 20
 CODE_GAL_E7Q = 21
 CODE_GAL_E7X = 22
 CODE_GAL_E8 = 23
-CODE_GAL_E5I = 24
-CODE_GAL_E5Q = 25
-CODE_GAL_E5X = 26
 
 CODE_QZS_L1CA = 27
 CODE_QZS_L2CM = 28
@@ -65,26 +65,42 @@ CODE_QZS_L5X = 33
 
 SUPPORTED_CODES = [CODE_GPS_L1CA,
                    CODE_GPS_L2CM,
+                   CODE_GPS_L2CL,
+                   CODE_GPS_L2CX,
                    CODE_GPS_L1P,
                    CODE_GPS_L2P,
+                   CODE_GPS_L5I,
+                   CODE_GPS_L5Q,
+                   CODE_GPS_L5X,
                    CODE_GLO_L1OF,
                    CODE_GLO_L2OF,
                    CODE_SBAS_L1CA,
                    CODE_BDS2_B11,
                    CODE_BDS2_B2,
-                   CODE_GPS_L5Q,
-                   CODE_QZS_L1CA,
-                   CODE_QZS_L2CM,
+                   CODE_GAL_E1B,
+                   CODE_GAL_E1C,
                    CODE_GAL_E1X,
+                   CODE_GAL_E5I,
+                   CODE_GAL_E5Q,
+                   CODE_GAL_E5X,
+                   CODE_GAL_E6B,
+                   CODE_GAL_E6C,
                    CODE_GAL_E6X,
+                   CODE_GAL_E7I,
+                   CODE_GAL_E7Q,
                    CODE_GAL_E7X,
                    CODE_GAL_E8,
-                   CODE_GAL_E5X]
+                   CODE_QZS_L1CA,
+                   CODE_QZS_L2CM,
+                   CODE_QZS_L2CL,
+                   CODE_QZS_L2CX,
+                   CODE_QZS_L5I,
+                   CODE_QZS_L5Q,
+                   CODE_QZS_L5X]
 
 L1CA_STR = 'GPS L1CA'
-L2CM_STR = 'GPS L2C'
-L2CX_STR = 'GPS L2C'
-L5Q_STR = 'GPS L5'
+L2C_STR = 'GPS L2C'
+L5_STR = 'GPS L5'
 L1P_STR = 'GPS L1P'
 L2P_STR = 'GPS L2P'
 SBAS_STR = 'SBAS L1'
@@ -92,33 +108,36 @@ L1OF_STR = 'GLO L1OF'
 L2OF_STR = 'GLO L2OF'
 BDS_B1_STR = 'BDS B1'
 BDS_B2_STR = 'BDS B2'
-QZS_L1CA_STR = 'QZSS L1CA'
-QZS_L2CM_STR = 'QZSS L2CM'
-QZS_L2CX_STR = 'QZSS L2C'
 GAL_E1_STR = 'GAL E1'
+GAL_E5_STR = 'GAL E5a'
 GAL_E6_STR = 'GAL E6'
 GAL_E7_STR = 'GAL E5b'
 GAL_E8_STR = 'GAL AltBOC'
-GAL_E5_STR = 'GAL E5a'
+QZS_L1CA_STR = 'QZS L1CA'
+QZS_L2C_STR = 'QZS L2C'
+QZS_L5_STR = 'QZS L5'
 
 CODE_TO_STR_MAP = {
     CODE_GPS_L1CA: L1CA_STR,
-    CODE_GPS_L2CM: L2CM_STR,
-    CODE_GPS_L2CX: L2CX_STR,
-    CODE_GPS_L5Q: L5Q_STR,
+    CODE_GPS_L2CM: L2C_STR,
+    CODE_GPS_L2CL: L2C_STR,
+    CODE_GPS_L2CX: L2C_STR,
     CODE_GPS_L1P: L1P_STR,
     CODE_GPS_L2P: L2P_STR,
-    CODE_SBAS_L1CA: SBAS_STR,
+    CODE_GPS_L5I: L5_STR,
+    CODE_GPS_L5Q: L5_STR,
+    CODE_GPS_L5X: L5_STR,
     CODE_GLO_L1OF: L1OF_STR,
     CODE_GLO_L2OF: L2OF_STR,
+    CODE_SBAS_L1CA: SBAS_STR,
     CODE_BDS2_B11: BDS_B1_STR,
     CODE_BDS2_B2: BDS_B2_STR,
-    CODE_QZS_L1CA: QZS_L1CA_STR,
-    CODE_QZS_L2CM: QZS_L2CM_STR,
-    CODE_QZS_L2CX: QZS_L2CX_STR,
     CODE_GAL_E1B: GAL_E1_STR,
     CODE_GAL_E1C: GAL_E1_STR,
     CODE_GAL_E1X: GAL_E1_STR,
+    CODE_GAL_E5I: GAL_E5_STR,
+    CODE_GAL_E5Q: GAL_E5_STR,
+    CODE_GAL_E5X: GAL_E5_STR,
     CODE_GAL_E6B: GAL_E6_STR,
     CODE_GAL_E6C: GAL_E6_STR,
     CODE_GAL_E6X: GAL_E6_STR,
@@ -126,16 +145,19 @@ CODE_TO_STR_MAP = {
     CODE_GAL_E7Q: GAL_E7_STR,
     CODE_GAL_E7X: GAL_E7_STR,
     CODE_GAL_E8: GAL_E8_STR,
-    CODE_GAL_E5I: GAL_E5_STR,
-    CODE_GAL_E5Q: GAL_E5_STR,
-    CODE_GAL_E5X: GAL_E5_STR
+    CODE_QZS_L1CA: QZS_L1CA_STR,
+    CODE_QZS_L2CM: QZS_L2C_STR,
+    CODE_QZS_L2CL: QZS_L2C_STR,
+    CODE_QZS_L2CX: QZS_L2C_STR,
+    CODE_QZS_L5I: QZS_L5_STR,
+    CODE_QZS_L5Q: QZS_L5_STR,
+    CODE_QZS_L5X: QZS_L5_STR
 }
 
 STR_TO_CODE_MAP = {
     L1CA_STR: CODE_GPS_L1CA,
-    L2CM_STR: CODE_GPS_L2CM,
-    L2CX_STR: CODE_GPS_L2CX,
-    L5Q_STR: CODE_GPS_L5Q,
+    L2C_STR: CODE_GPS_L2CM,
+    L5_STR: CODE_GPS_L5I,
     L1P_STR: CODE_GPS_L1P,
     L2P_STR: CODE_GPS_L2P,
     SBAS_STR: CODE_SBAS_L1CA,
@@ -143,14 +165,14 @@ STR_TO_CODE_MAP = {
     L2OF_STR: CODE_GLO_L2OF,
     BDS_B1_STR: CODE_BDS2_B11,
     BDS_B2_STR: CODE_BDS2_B2,
-    QZS_L1CA_STR: CODE_QZS_L1CA,
-    QZS_L2CM_STR: CODE_QZS_L2CM,
-    QZS_L2CX_STR: CODE_QZS_L2CX,
-    GAL_E1_STR: CODE_GAL_E1X,
-    GAL_E6_STR: CODE_GAL_E6X,
-    GAL_E7_STR: CODE_GAL_E7X,
+    GAL_E1_STR: CODE_GAL_E1B,
+    GAL_E5_STR: CODE_GAL_E5I,
+    GAL_E6_STR: CODE_GAL_E6B,
+    GAL_E7_STR: CODE_GAL_E7I,
     GAL_E8_STR: CODE_GAL_E8,
-    GAL_E5_STR: CODE_GAL_E5X
+    QZS_L1CA_STR: CODE_QZS_L1CA,
+    QZS_L2C_STR: CODE_QZS_L2CM,
+    QZS_L5_STR: CODE_QZS_L5I
 }
 
 CODE_NOT_AVAILABLE = 'N/A'
@@ -230,38 +252,36 @@ def code_is_bds2(code):
     return code in bds2_codes
 
 
-qzss_codes = {CODE_QZS_L1CA,
-              CODE_QZS_L2CM,
-              CODE_QZS_L2CL,
-              CODE_QZS_L2CX,
-              CODE_QZS_L5I,
-              CODE_QZS_L5Q,
-              CODE_QZS_L5X
-              }
-
-
-def code_is_qzss(code):
-    return code in qzss_codes
-
-
 gal_codes = {CODE_GAL_E1B,
              CODE_GAL_E1C,
              CODE_GAL_E1X,
+             CODE_GAL_E5I,
+             CODE_GAL_E5Q,
+             CODE_GAL_E5X,
              CODE_GAL_E6B,
              CODE_GAL_E6C,
              CODE_GAL_E6X,
              CODE_GAL_E7I,
              CODE_GAL_E7Q,
              CODE_GAL_E7X,
-             CODE_GAL_E8,
-             CODE_GAL_E5I,
-             CODE_GAL_E5Q,
-             CODE_GAL_E5X
-             }
+             CODE_GAL_E8}
 
 
 def code_is_galileo(code):
     return code in gal_codes
+
+
+qzss_codes = {CODE_QZS_L1CA,
+              CODE_QZS_L2CM,
+              CODE_QZS_L2CL,
+              CODE_QZS_L2CX,
+              CODE_QZS_L5I,
+              CODE_QZS_L5Q,
+              CODE_QZS_L5X}
+
+
+def code_is_qzss(code):
+    return code in qzss_codes
 
 
 def get_mode(msg):
