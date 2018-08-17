3be5ecabe810fcf5d89964a2bd5832e08e68cbbd
Keerthan Jaic <keerthan@swiftnav.com>
2018-08-17 11:17:12 -0700

    update macos compatibility notes

--- misc/create-dmg-installer.sh
+++ misc/create-dmg-installer.sh
@@ -112,7 +112,7 @@ echo "============================================="
 #cp -a "$RES"/background.png "$TMP_TEMPLATE"/.background
 
 mkdir -p "$TMP_TEMPLATE"
-echo "As of version 1.3 of the Swift Console, MacOS versions prior to Sierra are no longer supported. We apologize for any inconvenience." > $TMP_TEMPLATE/COMPATIBLE\ WITH\ MACOS\ 10.12\ AND\ NEWER.txt
+echo "As of version 2.0 of the Swift Console, MacOS versions prior to High Sierra are no longer supported. We apologize for any inconvenience." > $TMP_TEMPLATE/COMPATIBLE\ WITH\ MACOS\ 10.13\ AND\ NEWER.txt
 cp -a "$RES"/VolumeIcon.icns "$TMP_TEMPLATE"/.VolumeIcon.icns
 
 #cp -a "$RES"/DS_Store "$TMP_TEMPLATE"/.DS_Store
