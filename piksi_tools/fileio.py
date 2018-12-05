2e677462b6d0eca9c0b163fd9c45ecfd55bee781
Ben Altieri <benjaminaltieri@users.noreply.github.com>
2018-12-05 12:44:23 -0800

    Create tcp driver util and wrap error handling [ESD-632] (#925)
    * Create tcp driver util and wrap error handling
    
    * Update to libsbp 2.4.3
    
    * Rearrange imports for consistency

--- piksi_tools/fileio.py
+++ piksi_tools/fileio.py
@@ -19,13 +19,13 @@ import threading
 import sys
 
 from sbp.client import Framer, Handler
-from sbp.client.drivers.network_drivers import TCPDriver
 from sbp.file_io import (SBP_MSG_FILEIO_READ_DIR_RESP,
                          SBP_MSG_FILEIO_READ_RESP, SBP_MSG_FILEIO_WRITE_RESP,
                          MsgFileioReadDirReq, MsgFileioReadDirResp,
                          MsgFileioReadReq, MsgFileioRemove, MsgFileioWriteReq)
 
 from piksi_tools import serial_link
+from piksi_tools.utils import get_tcp_driver
 
 MAX_PAYLOAD_SIZE = 255
 SBP_FILEIO_WINDOW_SIZE = 100
@@ -459,11 +459,7 @@ def main():
     port = args.port[0]
     baud = args.baud[0]
     if args.tcp:
-        try:
-            host, port = port.split(':')
-            selected_driver = TCPDriver(host, int(port))
-        except:  # noqa
-            raise Exception('Invalid host and/or port')
+        selected_driver = get_tcp_driver(port)
     else:
         selected_driver = serial_link.get_driver(args.ftdi, port, baud)
 
