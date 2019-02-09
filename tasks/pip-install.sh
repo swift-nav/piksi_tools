1fea5f513487c93a2a8f0d3070a59f77557e51d3
Jason Mobarak <jason.mobarak@gmail.com>
2019-02-09 15:16:40 -0800

    Put setuptools_scm back

--- tasks/pip-install.sh
+++ tasks/pip-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Hack to make sure pip is always updated to the version specified
-pip install --upgrade pip==19.0.1
+pip install --upgrade pip==19.0.1 setuptools_scm
 
 pip install "$@"
