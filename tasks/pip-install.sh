1698da3e411ef78795b50c07e7f3c184265f2e3a
Jason Mobarak <jason.mobarak@gmail.com>
2019-02-09 16:28:12 -0800

    Fix-up linux workaround

--- tasks/pip-install.sh
+++ tasks/pip-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Hack to make sure pip is always updated to the version specified
-pip install --upgrade pip==19.0.1 setuptools_scm
+python -m pip install --upgrade pip==19.0.1 setuptools_scm
 
-pip install "$@"
+# Do the requested install
+python -m pip install "$@"
