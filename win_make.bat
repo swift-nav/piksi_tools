e443fb5d71adf191b7a764956e9b26cbb459c736
tnef <45163005+tn-exafore@users.noreply.github.com>
2019-04-04 11:02:25 +0300

    Switch win build config to python 3.5 and pyqt5 [EX-539]

--- win_make.bat
+++ win_make.bat
@@ -1,7 +1,7 @@
 echo off
 setlocal
-c:\python27\python setup.py develop
-c:\python27\python setup.py install
-SET PATH=%PATH%;C:\cygwin\bin;C:\Program Files (x86)\NSIS;C:\Program Files\NSIS;C:\Python27\DLLs;C:\Python27\Scripts;C:\Python27\Lib\site-packages\vtk;C:\Python27\gnuplot\binary;C:\Python27\Lib\site-packages\osgeo;C:\Program Files (x86)\pythonxy\SciTE-3.5.1-4;C:\Program Files (x86)\pythonxy\console;C:\Program Files (x86)\pythonxy\swig;C:\Program Files (x86)\pythonxy\gettext\bin;
+c:\python35\python setup.py develop
+c:\python35\python setup.py install
+SET PATH=%PATH%;C:\cygwin\bin;C:\Program Files (x86)\NSIS;C:\Program Files\NSIS;C:\Python35\DLLs;C:\Python35\Scripts;C:\Python35\Lib\site-packages\vtk;C:\Python35\gnuplot\binary;C:\Python35\Lib\site-packages\osgeo;C:\Program Files (x86)\pythonxy\SciTE-3.5.1-4;C:\Program Files (x86)\pythonxy\console;C:\Program Files (x86)\pythonxy\swig;C:\Program Files (x86)\pythonxy\gettext\bin;
 python piksi_tools/version.py
 make build_console
