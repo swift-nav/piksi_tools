66c3a106beb1b3c67c6dae89809c8134c4ac0618
tnef <45163005+tn-exafore@users.noreply.github.com>
2019-04-26 12:37:43 +0300

    Remove a broken optimization to fix computed doppler [ESD-1276]
    Otherwise local computed doppler is missing when setting
    (soln freq) / (output every n obs) is high. The optimization was
    also otherwise broken in that it didn't update TOW as advertised.
    
    Also, UI updates are nowadays separately scheduled so this
    doesn't directly affect UI update frequency.

--- piksi_tools/console/observation_view.py
+++ piksi_tools/console/observation_view.py
@@ -170,10 +170,7 @@ class ObservationView(CodeFiltered):
             return
         else:
             self.prev_obs_count = count
-        # Don't bother updating anything except the TOW faster than 2Hz
-        tow_diff = self.gps_tow - self.last_table_update_tow
-        if tow_diff > 0 and tow_diff < 0.5:
-            return
+
         # Save this packet
         # See sbp_piksi.h for format
         for o in sbp_msg.obs:
