663bdc3bc8884de766a5880962852dea37041792
Kevin Dade <kevin@swiftnav.com>
2019-05-08 10:52:50 -0700

    Mostly seems to work except for plotting.

--- piksi_tools/console/attitude_view.py
+++ piksi_tools/console/attitude_view.py
@@ -64,60 +64,40 @@ class AttitudeView(HasTraits):
         # SBP Console stuff.
         self.link = link
         self.link.add_callback(self.callback_orient_euler, SBP_MSG_ORIENT_EULER)
-        #self.link.add_callback(self.callback_imu_raw, SBP_MSG_IMU_RAW)
         self.python_console_cmds = {'track': self}
 
         # Plot stuff.
-        self.init_plot_data()
+        self.init_plotting()
 
-    def init_plot_data(self):
-        self.acc_x = np.zeros(NUM_PLOT_POINTS)
-        self.acc_y = np.zeros(NUM_PLOT_POINTS)
-        self.acc_z = np.zeros(NUM_PLOT_POINTS)
-        self.gyro_x = np.zeros(NUM_PLOT_POINTS)
-        self.gyro_y = np.zeros(NUM_PLOT_POINTS)
-        self.gyro_z = np.zeros(NUM_PLOT_POINTS)
+    def init_plotting(self):
+        self.std_y = np.zeros(NUM_PLOT_POINTS)
+        self.std_p = np.zeros(NUM_PLOT_POINTS)
+        self.std_r = np.zeros(NUM_PLOT_POINTS)
         self.last_plot_update_time = 0
         self.data = ArrayPlotData(t=np.arange(NUM_PLOT_POINTS),
-                                  acc_x=[0.0],
-                                  acc_y=[0.0],
-                                  acc_z=[0.0],
-                                  gyr_x=[0.0],
-                                  gyr_y=[0.0],
-                                  gyr_z=[0.0])
+                                  std_y=[0.0],
+                                  std_p=[0.0],
+                                  std_r=[0.0])
         self.plot = Plot(self.data, auto_colors=[0x0000FF, 0x00FF00, 0xFF0000], emphasized=True)
-        self.plot.title = 'Raw IMU Data'
+        self.plot.title = 'Attitude Uncertainty'
         self.plot.title_color = [0, 0, 0.43]
         self.ylim = self.plot.value_mapper.range
         self.ylim.low = -32768
         self.ylim.high = 32767
-        # self.plot.value_range.bounds_func = lambda l, h, m, tb: (0, h * (1 + m))
         self.plot.value_axis.orientation = 'right'
         self.plot.value_axis.axis_line_visible = False
         self.plot.value_axis.title = 'LSB count'
-
         self.legend_visible = True
         self.plot.legend.visible = True
         self.plot.legend.align = 'll'
         self.plot.legend.line_spacing = 1
         self.plot.legend.font = 'modern 8'
         self.plot.legend.draw_layer = 'overlay'
-        self.plot.legend.tools.append(
-            LegendTool(self.plot.legend, drag_button="right"))
+        self.plot.legend.tools.append(LegendTool(self.plot.legend, drag_button="right"))
 
-        acc_x = self.plot.plot(
-            ('t', 'acc_x'), type='line', color='auto', name='Accn. X')
-        acc_x = self.plot.plot(
-            ('t', 'acc_y'), type='line', color='auto', name='Accn. Y')
-        acc_x = self.plot.plot(
-            ('t', 'acc_z'), type='line', color='auto', name='Accn. Z')
-        acc_x = self.plot.plot(
-            ('t', 'gyr_x'), type='line', color='auto', name='Gyro X')
-        acc_x = self.plot.plot(
-            ('t', 'gyr_y'), type='line', color='auto', name='Gyro Y')
-        acc_x = self.plot.plot(
-            ('t', 'gyr_z'), type='line', color='auto', name='Gyro Z')
-
+        std_y = self.plot.plot(('t', 'std_y'), type='line', color='auto', name='Yaw \sigma')
+        std_p = self.plot.plot(('t', 'std_p'), type='line', color='auto', name='Pitch \sigma')
+        std_r = self.plot.plot(('t', 'std_r'), type='line', color='auto', name='Roll \sigma')
 
     @on_trait_change('scene.activated')
     def initialize_scene(self):
@@ -144,27 +124,26 @@ class AttitudeView(HasTraits):
     # Update the attitude representation.
     def update_attitude(self, y, p, r):
         self.init_tform_ned()
+        print("ATTITUDE", np.deg2rad(r), np.deg2rad(p), np.deg2rad(y))
         self.tform.rotate_x(r)
         self.tform.rotate_y(p)
         self.tform.rotate_z(y)
 
         self.axes.user_transform = self.tform
-        self.update_scheduler.schedule_update("render_attitude", self.scene.render)
+        #self.update_scheduler.schedule_update("render_attitude", self.scene.render)
+        self.scene.render()
+
 
     def update_attitude_std(self, y_std, p_std, r_std):
         pass
 
     def callback_orient_euler(self, sbp_msg, **metadata):
-        y = sbp_msg.yaw / 10.e6
-        p = sbp_msg.pitch / 10.e6 
-        r = sbp_msg.roll / 10.e6
+        y = sbp_msg.yaw / 1.e6
+        p = sbp_msg.pitch / 1.e6 
+        r = sbp_msg.roll / 1.e6
         y_std = sbp_msg.yaw_accuracy
         p_std = sbp_msg.pitch_accuracy
         r_std = sbp_msg.roll_accuracy
         self.update_attitude(y, p, r)
         self.update_attitude_std(y_std, p_std, r_std)
-
-    def callback_imu_raw(self, sbp_msg, **metadata):
-        print("Got an IMU Raw!")
-        self.update_attitude(360*np.random.rand(), 360*np.random.rand(), 360*np.random.rand())
 
