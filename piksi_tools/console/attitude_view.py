dbd938e38b2dd80359deb252af40e18e1a57f9c7
Kevin Dade <kevin@swiftnav.com>
2019-05-08 14:28:57 -0700

    Add a bias plot.

--- piksi_tools/console/attitude_view.py
+++ piksi_tools/console/attitude_view.py
@@ -65,9 +65,12 @@ class AttitudeView(HasTraits):
     axes = Instance(tvtk.AxesActor, ())
     logo = Instance(tvtk.Actor, ())
 
-    # Plot objects for stddev.
-    plot = Instance(Plot)
-    data = Instance(ArrayPlotData)
+    # Plot objects.
+    angle_plot = Instance(Plot)
+    angle_data = Instance(ArrayPlotData)
+
+    bias_plot = Instance(Plot)
+    bias_data = Instance(ArrayPlotData)
 
     ##################################################################
     traits_view =   View(
@@ -76,10 +79,16 @@ class AttitudeView(HasTraits):
                                  editor=SceneEditor(scene_class=AttitudeScene),
                                  show_label=False,
                                  resizable=True),
-                            Item(name='plot',
-                                 editor=ComponentEditor(bgcolor=(.8,.8,.8)),
-                                 show_label=False,
-                                 resizable=True)
+                            VGroup(
+                                Item(name='angle_plot',
+                                     editor=ComponentEditor(bgcolor=(.8,.8,.8)),
+                                     show_label=False,
+                                     resizable=True),
+                                Item(name='bias_plot',
+                                     editor=ComponentEditor(bgcolor=(.8,.8,.8)),
+                                     show_label=False,
+                                     resizable=True)
+                            )
                         ),
                         resizable=True,
                         scrollable=False
@@ -98,34 +107,64 @@ class AttitudeView(HasTraits):
         self.init_plotting()
 
     def init_plotting(self):
+        self.last_plot_update_time = 0
+        # Set up the angle plotting stuff.
         self.est_yaw = np.zeros(NUM_PLOT_POINTS)
         self.est_pit = np.zeros(NUM_PLOT_POINTS)
         self.est_rol = np.zeros(NUM_PLOT_POINTS)
-        self.last_plot_update_time = 0
-        self.data = ArrayPlotData(t=np.arange(NUM_PLOT_POINTS),
-                                  est_yaw=[0.0],
-                                  est_pit=[0.0],
-                                  est_rol=[0.0])
-        self.plot = Plot(self.data, auto_colors=[0x0000FF, 0x00FF00, 0xFF0000], emphasized=True)
-        self.plot.title = 'Attitude Uncertainty'
-        self.plot.title_color = [0, 0, 0.43]
-        self.ylim = self.plot.value_mapper.range
+        self.angle_data = ArrayPlotData(t=np.arange(NUM_PLOT_POINTS),
+                                        est_yaw=[0.0],
+                                        est_pit=[0.0],
+                                        est_rol=[0.0])
+        self.angle_plot = Plot(self.angle_data, auto_colors=[0x0000FF, 0x00FF00, 0xFF0000], emphasized=True)
+        self.angle_plot.title = 'Yaw/Pitch/Roll'
+        self.angle_plot.title_color = [0, 0, 0.43]
+        self.ylim = self.angle_plot.value_mapper.range
         self.ylim.low = -180.
         self.ylim.high = 180. 
-        self.plot.value_axis.orientation = 'right'
-        self.plot.value_axis.axis_line_visible = False
-        self.plot.value_axis.title = 'Angle'
+        self.angle_plot.value_axis.orientation = 'right'
+        self.angle_plot.value_axis.axis_line_visible = False
+        self.angle_plot.value_axis.title = 'Angle'
         self.legend_visible = True
-        self.plot.legend.visible = True
-        self.plot.legend.align = 'll'
-        self.plot.legend.line_spacing = 1
-        self.plot.legend.font = 'modern 8'
-        self.plot.legend.draw_layer = 'overlay'
-        self.plot.legend.tools.append(LegendTool(self.plot.legend, drag_button="right"))
+        self.angle_plot.legend.visible = True
+        self.angle_plot.legend.align = 'll'
+        self.angle_plot.legend.line_spacing = 1
+        self.angle_plot.legend.font = 'modern 8'
+        self.angle_plot.legend.draw_layer = 'overlay'
+        self.angle_plot.legend.tools.append(LegendTool(self.angle_plot.legend, drag_button="right"))
 
-        self.plot.plot(('t', 'est_yaw'), type='line', color='auto', name='Yaw (\degree)')
-        self.plot.plot(('t', 'est_pit'), type='line', color='auto', name='Pitch (\degree)')
-        self.plot.plot(('t', 'est_rol'), type='line', color='auto', name='Roll (\degree)')
+        self.angle_plot.plot(('t', 'est_yaw'), type='line', color='auto', name='Yaw (\degree)')
+        self.angle_plot.plot(('t', 'est_pit'), type='line', color='auto', name='Pitch (\degree)')
+        self.angle_plot.plot(('t', 'est_rol'), type='line', color='auto', name='Roll (\degree)')
+
+        # Set up the bias plotting stuff.
+        self.bias_x = np.zeros(NUM_PLOT_POINTS)
+        self.bias_y = np.zeros(NUM_PLOT_POINTS)
+        self.bias_z = np.zeros(NUM_PLOT_POINTS)
+        self.bias_data = ArrayPlotData(t=np.arange(NUM_PLOT_POINTS),
+                                       bias_z=[0.0],
+                                       bias_y=[0.0],
+                                       bias_x=[0.0])
+        self.bias_plot = Plot(self.bias_data, auto_colors=[0x0000FF, 0x00FF00, 0xFF0000], emphasized=True)
+        self.bias_plot.title = 'Gyroscope Biases'
+        self.bias_plot.title_color = [0, 0, 0.43]
+        self.ylim = self.bias_plot.value_mapper.range
+        self.ylim.low = -.1
+        self.ylim.high = .1 
+        self.bias_plot.value_axis.orientation = 'right'
+        self.bias_plot.value_axis.axis_line_visible = False
+        self.bias_plot.value_axis.title = 'rad/s'
+        self.legend_visible = True
+        self.bias_plot.legend.visible = True
+        self.bias_plot.legend.align = 'll'
+        self.bias_plot.legend.line_spacing = 1
+        self.bias_plot.legend.font = 'modern 8'
+        self.bias_plot.legend.draw_layer = 'overlay'
+        self.bias_plot.legend.tools.append(LegendTool(self.bias_plot.legend, drag_button="right"))
+
+        self.bias_plot.plot(('t', 'bias_z'), type='line', color='auto', name='\omega_z bias')
+        self.bias_plot.plot(('t', 'bias_y'), type='line', color='auto', name='\omega_y bias')
+        self.bias_plot.plot(('t', 'bias_x'), type='line', color='auto', name='\omega_x bias')
 
     @on_trait_change('scene.activated')
     def initialize_scene(self):
@@ -143,10 +182,6 @@ class AttitudeView(HasTraits):
         self.logo = _secret("./SwiftNavLogo-Square.png")
         self.scene.add_actor(self.axes)
         self.scene.add_actor(self.logo)
-        # help(self.axes)
-        # Set up the camera. Look down on the origin from above.
-        #self.scene.scene_editor._camera.position = (0., 0., 10.)
-        #self.scene.scene_editor._renderer.reset_camera()
 
     def init_tform_ned(self):
         self.tform.identity()
@@ -183,19 +218,37 @@ class AttitudeView(HasTraits):
 
         # Update the attitude view at full rate.
         self.update_scheduler.schedule_update("render_attitude", self.scene.render)
-        # Update the plots at reduced rate.
-        if monotonic() - self.last_plot_update_time >= GUI_UPDATE_PERIOD:
-            self.update_scheduler.schedule_update("attitude_plot_update", self.update_plot)
 
-    def update_plot(self):
+    def update_biases(self, bx, by, bz):
+        memoryview(self.bias_x)[:-1] = memoryview(self.bias_x)[1:]
+        memoryview(self.bias_y)[:-1] = memoryview(self.bias_y)[1:]
+        memoryview(self.bias_z)[:-1] = memoryview(self.bias_z)[1:]
+        self.bias_x[-1] = bx 
+        self.bias_y[-1] = by 
+        self.bias_z[-1] = bz 
+
+    def update_plots(self):
         self.last_plot_update_time = monotonic()
-        self.data.set_data('est_yaw', self.est_yaw)
-        self.data.set_data('est_pit', self.est_pit)
-        self.data.set_data('est_rol', self.est_rol)
-        
+
+        self.angle_data.set_data('est_yaw', self.est_yaw)
+        self.angle_data.set_data('est_pit', self.est_pit)
+        self.angle_data.set_data('est_rol', self.est_rol)
+
+        self.bias_data.set_data('bias_x', self.bias_x)
+        self.bias_data.set_data('bias_y', self.bias_y)
+        self.bias_data.set_data('bias_z', self.bias_z)
+       
     def callback_orient_euler(self, sbp_msg, **metadata):
         y = sbp_msg.yaw / 1.e6
         p = sbp_msg.pitch / 1.e6 
         r = sbp_msg.roll / 1.e6
+        bx = sbp_msg.yaw_accuracy
+        by = sbp_msg.pitch_accuracy
+        bz = sbp_msg.roll_accuracy
         self.update_attitude(y, p, r)
+        self.update_biases(bx, by, bz)
+        # Update the plots at reduced rate.
+        if monotonic() - self.last_plot_update_time >= GUI_UPDATE_PERIOD:
+            self.update_scheduler.schedule_update("attitude_plot_update", self.update_plots)
+
 
