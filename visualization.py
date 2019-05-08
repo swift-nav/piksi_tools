da824b49b3e0925326589a08656e387c3f06a901
Kevin Dade <kevin@swiftnav.com>
2019-05-07 21:57:17 -0700

    Almost there! Temporary solution stored in visualization.py

--- visualization.py
+++ visualization.py
@@ -11,9 +11,6 @@ from tvtk.pyface import actors
 from tvtk.pyface.scene import Scene
 from mayavi.core.ui.api import MlabSceneModel, SceneEditor
 
-
-from .gui_utils import GUI_UPDATE_PERIOD, UpdateScheduler
-
 ######################################################################
 class AttitudeScene(Scene):
     def _actions_default(self):
@@ -24,6 +21,9 @@ class AttitudeView(HasTraits):
     python_console_cmds = Dict()
     # The scene model.
     scene = Instance(MlabSceneModel, ())
+
+    # The axes transform.
+    tform = Instance(tvtk.Transform, ())
 
     ##################################################################
     traits_view = View(Item(name='scene',
@@ -36,35 +36,36 @@ class AttitudeView(HasTraits):
     @on_trait_change('scene.activated')
     def initialize_scene(self):
         # Turn off all interaction.
-        self.scene.scene_editor._interactor.interactor_style = None
-        # Add some actors.
-        sphere = actors.sphere_actor(center=[0., 0., 0.], radius=1.)
-        tf = tvtk.Transform()
-        tf.pre_multiply()
-        #tf.translate(0., 0., 1.)
-        #tf.rotate_y(180.)
-        #tf.rotate_x(90.)
-        axes = tvtk.AxesActor(user_transform=tf,
-                              cylinder_radius=.02,
-                              shaft_type='cylinder')
-        print("RADIUS", axes.cylinder_radius)
-        print("SHAFT", axes.shaft_type)
+        #self.scene.scene_editor._interactor.interactor_style = None
+        self.tform = tvtk.Transform()
+        axes = tvtk.AxesActor(cylinder_radius=.02,
+                              shaft_type='cylinder',
+                              axis_labels=False,
+                              user_transform=self.tform)
+        self.tform.rotate_y(180.)
+        self.tform.rotate_x(90.)
+        self.tform.push()
         self.scene.add_actor(axes)
         # Set up the camera. Look down on the origin from above.
-        #self.scene.scene_editor._camera.position = (0, -3., 3.)
-        #self.scene.scene_editor._camera.elevation()
-        self.scene.scene_editor._renderer.reset_camera()
-        self.scene.scene_editor._renWin.render()
+        #self.scene.scene_editor._camera.position = (0., 0., 10.)
+        #self.scene.scene_editor._renderer.reset_camera()
 
 
-    def __init__(self, link, **traits):
+    def __init__(self, **traits):
         HasTraits.__init__(self, **traits)
     
         # SBP Console stuff.
-        self.link = link
+        #self.link = link
         #self.link.add_callback(self.imu_raw_callback, SBP_MSG_IMU_RAW)
         #self.link.add_callback(self.imu_aux_callback, SBP_MSG_IMU_AUX)
 
         self.python_console_cmds = {'track': self}
         #self.update_scheduler = UpdateScheduler()
+class Test(HasTraits):
+    scene = Instance(AttitudeView, ())
+
+    view = View(Item(name='scene', style='custom', show_label=False))
+
+v = Test()
+v.configure_traits()
 
