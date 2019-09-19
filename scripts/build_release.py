16cfbdb698a8d0cf4184006d2c074b07efa1dd36
Jason Mobarak <jason.mobarak@gmail.com>
2019-09-19 10:38:00 -0700

    Build automation support for Ubuntu 18.04 (#1077) [1/2]
    + Add docker image for building Ubuntu 18.04 GUI-- on Ubuntu 18.04,
      we use the default Python 3, which is 3.6.  We dropped Python 2
      support for the Console because of compatibility issues that
      could not be easily resolved.
    
    + Add intial work to support for ARM version of console Ubuntu 18.04,
      not enabled yet.
    
      ARM support means lots of building of really big libraries ourselves.
      We need to figure out how to properly store and maintain these
      builds over time.  The ARM support involes building Qt in particular,
      which has a very length build process.
    
    + Remove Travis "artifacts" add-on and replace with direct invocations
      of AWS CLI, the artifacts was not working during work on this PR
      (and we could not figure out why)-- so AWS CLI was used as work
      around.  This makes piksi_tools more consistent with other repos
      and allows us to give uploaded artifacts saner names.

--- scripts/build_release.py
+++ scripts/build_release.py
@@ -5,7 +5,15 @@ import os
 import shutil
 import subprocess
 import sys
+import tarfile
+import zipfile
+
 from subprocess import check_call, check_output, CalledProcessError
+
+from six.moves import reload_module
+
+BIONIC_DOCKER_TAG = 'swiftnav/piksi-tools-bionic:2019.06.20'
+S3_BUCKET = 'console-ci-builds'
 
 
 def maybe_remove(path):
@@ -20,45 +28,119 @@ def _check_output(cmd, default=None):
     except CalledProcessError as cpe:
         print("Output:\n" + cpe.output)
         print("Return Code:\n" + str(cpe.returncode))
-        raise CalledProcessError
+        raise cpe
     return default
 
 
-def build(env):
-    from six.moves import reload_module
-    check_call(['tox', '-e', env])
-    out_pyi = os.path.join(os.getcwd(), os.path.join('dist', 'console'))
+def get_version():
     # tox writes _version.py, doing a delayed import and a reload after that
     from piksi_tools import _version
     reload_module(_version)
-    ver = _version.version
+    return _version.version
+
+
+def build(env):
+    check_call(['tox', '-e', env, '-vv'])
+    out_pyi = os.path.join(os.getcwd(), os.path.join('dist', 'console'))
+    ver = get_version()
     print("build version:", ver)
     return out_pyi, ver
 
 
 def build_linux():
-    import tarfile
+
+    print('>>> Building console for Generic Linux')
+
     out_pyi, version = build('pyinstaller-linux')
-    out = os.path.join(os.getcwd(), os.path.join('dist', 'swift_console_{}_linux'.format(version)))
+    out_dist = os.path.join('dist', 'swift_console_{}_linux'.format(version))
+    out = os.path.join(os.getcwd(), out_dist)
     maybe_remove(out)
+
+    print(">>> Moving '{}' to '{}'...".format(out_pyi, out))
     shutil.move(out_pyi, out)
 
     # prevents missing export error on ubuntu 16.04 with intel graphics
     os.remove(os.path.join(out, 'libdrm.so.2'))
 
-    print('Creating tar archive')
-    with tarfile.open(out + '.tar.gz', 'w:gz') as tar:
+    fname = '{}.tar.gz'.format(out)
+
+    print('>>> Creating tar archive Generic Linux')
+    with tarfile.open(fname, 'w:gz') as tar:
         tar.add(out, arcname=os.path.basename(out))
+
+    print('>>> Listing dist dir:')
+    print(os.listdir("dist"))
+
+    s3_fname = "{}.tar.gz".format(os.path.basename(out_dist))
+    s3_path = 's3://{}/{}/linux_generic/{}'.format(S3_BUCKET, version, s3_fname)
+
+    print(">>> Uploading to {}".format(s3_path))
+    check_call(['aws', 's3', 'cp', fname, s3_path])
+
+
+
+def build_linux_bionic():
+
+    print('>>> Building console for Ubuntu Bionic (18.04)')
+
+    cwd = os.getcwd()
+    check_call(['docker', 'run', '-e', 'AWS_SECRET_ACCESS_KEY', '-e', 'AWS_ACCSS_KEY_ID', '-e', 'AWS_DEFAULT_REGION',
+                '-v', cwd + ':/work', '-it', '--rm', BIONIC_DOCKER_TAG,
+                'tox', '-e', 'pyinstaller36-linux', '-vv'])
+
+    version = get_version()
+    print("build version:", version)
+
+    out_dist = os.path.join('dist', 'swift_console_{}_ubuntu1804'.format(version))
+    out = os.path.join(os.getcwd(), out_dist)
+    out_pyi = os.path.join(os.getcwd(), os.path.join('dist', 'console'))
+
+    uid, gid = os.getuid(), os.getgid()
+    check_call(['sudo', 'chown', '-R', '{}:{}'.format(uid, gid), out_pyi, 'dist'])
+
+    print(">>> Moving '{}' to '{}'...".format(out_pyi, out))
+
+    maybe_remove(out)
+    shutil.move(out_pyi, out)
+
+    fname = '{}.tar.gz'.format(out)
+
+    print('>>> Creating tar archive for Ubuntu Bionic: ' + fname)
+    with tarfile.open(fname, 'w:gz') as tar:
+        tar.add(out, arcname=os.path.basename(out))
+
+    print('>>> Listing dist dir:')
+    print(os.listdir("dist"))
+
+    s3_fname = "{}.tar.gz".format(os.path.basename(out_dist))
+    s3_path = 's3://{}/{}/linux_ubuntu_1804/{}'.format(S3_BUCKET, version, s3_fname)
+
+    print(">>> Uploading to {}".format(s3_path))
+    check_call(['aws', 's3', 'cp', fname, s3_path])
 
 
 def build_macos():
     out, version = build('pyinstaller-macos')
+    fname = 'swift_console_{}_macos.dmg'.format(version)
     check_call([
         'sudo',
-        os.path.join(os.getcwd(), os.path.join('misc',
-                     'create-dmg-installer.sh')),
-        'swift_console_{}_macos.dmg'.format(version)
+        os.path.join(os.getcwd(), os.path.join('misc', 'create-dmg-installer.sh')),
+        fname 
     ])
+
+    dmg_rel_path = "dist/{}".format(fname)
+
+    print(">>> Fix .dmg file owner:group info")
+    uid, gid = os.getuid(), os.getgid()
+    check_call(['sudo', 'chown', '{}:{}'.format(uid, gid), dmg_rel_path])
+
+    print(">>> Verify AWS CLI tool is in path")
+    check_call(['command', '-v', 'aws'])
+
+    s3_path = 's3://{}/{}/darwin/{}'.format(S3_BUCKET, version, fname)
+
+    print(">>> Uploading to {}".format(s3_path))
+    check_call(['aws', 's3', 'cp', dmg_rel_path, s3_path])
 
 
 def build_win():
@@ -80,19 +162,37 @@ def build_win():
     ])
 
 
-def build_cli_tools():
+def zipdir(path, ziph):
+    # https://stackoverflow.com/questions/1855095/how-to-create-a-zip-archive-of-a-directory-in-python
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            arcname = os.path.relpath(os.path.join(root, file), os.path.join(path, '..')) 
+            ziph.write(os.path.join(root, file), arcname)
+
+
+def build_cli_tools(plat):
+    version = get_version()
     _check_output(['tox', '-e', 'pyinstaller_cmdline_tools'])
+    out = os.path.join('dist', 'cmd_line')
+    fname = 'cmdline_tools_{}.zip'.format(version)
+    if not plat.startswith('win'):
+        with zipfile.ZipFile(fname, 'w') as ziph:
+            zipdir(out, ziph)
+        s3_path = 's3://{}/{}/{}/{}'.format(S3_BUCKET, version, plat, fname)
+        print(">>> Uploading to {}".format(s3_path))
+        check_call(['aws', 's3', 'cp', fname, s3_path])
 
 
 def main():
     plat = sys.platform
     if plat.startswith('linux'):
         build_linux()
+        build_linux_bionic()
     elif plat.startswith('darwin'):
         build_macos()
     elif plat.startswith('win'):
         build_win()
-    build_cli_tools()
+    build_cli_tools(plat)
 
 
 if __name__ == '__main__':
