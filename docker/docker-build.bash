907c3afa6d5ab72443bb920199cb3b3e1407ed82
Jason Mobarak <jason.mobarak@gmail.com>
2019-09-19 10:38:00 -0700

    Build automation support for Ubuntu 18.04 (#1077) [1/2]
    + Add docker image for building Ubuntu 18.04 GUI-- on Ubuntu 18.04,
      we use the default Python 3, which is 3.6.  We dropped Python 2
      support for the Console because of compatibility issues that
      could not be easily resolved.
    
    + Add intial work to support for ARM version of console Ubuntu 18.04,
      not enabled yet.
    
      ARM support means lots of building of really big libraries ourselves.
      We need to figure out how to properly store and maintain these
      builds over time.  The ARM support involes building Qt in particular,
      which has a very length build process.
    
    + Remove Travis "artifacts" add-on and replace with direct invocations
      of AWS CLI, the artifacts was not working during work on this PR
      (and we could not figure out why)-- so AWS CLI was used as work
      around.  This makes piksi_tools more consistent with other repos
      and allows us to give uploaded artifacts saner names.

---
+++ docker/docker-build.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+D=$( (cd "$(dirname "$0")" || exit 1 >/dev/null; pwd -P) )
+
+VERSION_TAG=$(cat "$D/docker_version_tag")
+DOCKER_REPO_NAME=swiftnav/piksi-tools-bionic
+DOCKER_USER=swiftnav
+DOCKER_FILE=Dockerfile
+
+echo '>>> Running docker build command...'
+cd "$D/.."
+
+if [[ -z "${USE_CACHE:-}" ]];then
+  docker build \
+    --force-rm \
+    --no-cache \
+    -f "$D/$DOCKER_FILE" \
+    -t "$DOCKER_REPO_NAME:$VERSION_TAG" \
+    .
+else
+  docker build \
+    -f "$D/$DOCKER_FILE" \
+    -t "$DOCKER_REPO_NAME:$VERSION_TAG" \
+    .
+fi
+
+echo '>>> Pushing build to Docker Hub...'
+
+if [[ -n "${DOCKER_PASS:-}" ]]; then
+  echo "$DOCKER_PASS" | docker login --username="${DOCKER_USER:-swiftnav}" --password-stdin
+  docker push "$DOCKER_REPO_NAME:$VERSION_TAG"
+else
+  echo "WARNING: Not pushing new image to Docker Hub"
+fi
