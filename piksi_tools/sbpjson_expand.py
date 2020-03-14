66d08709babf2e5d9e9a74bab524d98631f16e77
Pasi Miettinen <pmiettinen@exafore.com>
2020-03-14 09:24:36 +0200

    Playback support in serial_link (#1138)
    * Add JSON log iterator as possible source
    
    * Script for expanding SBP JSON payloads
    
    * Use common JSONLogIterator
    
    * sbpjson_expand.py: Use conventional JSONLogIterator
    
    * Update sbp requirement to 2.7.7 to include playback feature
    
    * Add playback option to serial_link
    
    Co-authored-by: Jason Mobarak <jason@swift-nav.com>

---
+++ piksi_tools/sbpjson_expand.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# Copyright (C) 2020 Swift Navigation Inc.
+# Contact: Swift Navigation <dev@swiftnav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+import argparse
+import os
+
+from sbp.client.loggers.json_logger import JSONLogger, JSONLogIterator
+
+
+def get_args():
+    """
+    Get and parse arguments.
+    """
+    parser = argparse.ArgumentParser(description='SBP JSON payload expander')
+
+    parser.add_argument(
+        'filename',
+        help="The SBP log file to extract data from. Default format is sbp json")
+
+    parser.add_argument(
+        '-i',
+        '--ignore',
+        default=[],
+        type=int,
+        nargs='*',
+        help="Filter message types")
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = get_args()
+    logger = JSONLogger(None)
+
+    name, ext = os.path.splitext(args.filename)
+
+    outfile = "{name}-expanded{ext}".format(name=name, ext=ext)
+
+    with open(args.filename, 'r') as infile, open(outfile, 'w') as outfile:
+        for (msg, meta) in JSONLogIterator(infile, conventional=True):
+
+            if msg.msg_type in args.ignore:
+                continue
+
+            outfile.write(logger.dump(msg))
+            outfile.write('\n')
+
+
+if __name__ == '__main__':
+    main()
